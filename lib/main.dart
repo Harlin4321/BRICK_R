ff38977c1bb90d0afbd9dada67b829333ac21649
Harlin4321 <kr70136508@gmail.com>
2025-10-06 10:01:40 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:brick_app/screens/splash_screen.dart';
+import 'screens/splash_screen.dart'; // Make sure this path exists
 
 void main() {
   runApp(const MyApp());
@@ -12,42 +12,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Brick App',
       debugShowCheckedModeBanner: false,
+      title: 'Brick App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      supportedLocales: const [
+        Locale('en', 'US'),
+      ],
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [
-        Locale('en', ''),
-      ],
-      theme: ThemeData(
-        useMaterial3: true, // Important for Flutter 3+
-        primarySwatch: Colors.blue,
-        fontFamily: 'Roboto', // Default font for body text
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 32,
-              fontWeight: FontWeight.bold), // Heading 1
-          displayMedium: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 24,
-              fontWeight: FontWeight.bold), // Heading 2
-          displaySmall: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 20,
-              fontWeight: FontWeight.bold), // Heading 3
-          bodyLarge: TextStyle(fontSize: 16), // Roboto body
-          bodyMedium: TextStyle(fontSize: 14),
-          labelLarge: TextStyle(fontWeight: FontWeight.bold), // Buttons
-        ),
-      ),
-      home: const SplashScreen(), // Starting screen
+      home: const SplashScreen(),
     );
   }
 }
+
 
 
 
