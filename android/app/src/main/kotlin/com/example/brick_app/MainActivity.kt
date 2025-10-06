265ec9eefb18d53be4cbcc67129db3f0ce04ef94
Harlin4321 <kr70136508@gmail.com>
2025-10-07 00:08:25 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/brick_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/brick_app/MainActivity.kt
@@ -1,10 +1,9 @@
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+package com.example.brick_r
+
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
+    // No need to override onCreate
 }
+
 
