a631e149f110f4253a249bbf0e2b5b3b4f186afa
Harlin4321 <kr70136508@gmail.com>
2025-10-08 00:42:07 +0530

    Update MainActivity.java

--- android/src/main/java/com/example/brick_r/MainActivity.java
+++ android/src/main/java/com/example/brick_r/MainActivity.java
@@ -3,5 +3,6 @@ package com.example.brick_r;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-    // v2 embedding — NO registerWith() or onCreate()
+    // v2 embedding — no registerWith() or onCreate() overrides
 }
+
